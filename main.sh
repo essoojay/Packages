c420981a84b824b8983b1733d125e07e45fbdad0
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2022-02-19 14:02:54 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# NueXini
+
+####################################################################################################
+
+# luci-app-nezha 哪吒监控
+svn co https://github.com/Erope/openwrt_nezha/trunk/luci-app-nezha ./luci-app-nezha
+svn co https://github.com/Erope/openwrt_nezha/trunk/openwrt-nezha ./openwrt-nezha
+rm -rf .svn
+####################################################################################################
